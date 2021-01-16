3436ec03c0670c578889e7547eaf8fcd5db77f51
Yiheng Xu <yhxu@umd.edu>
2021-01-16 13:12:17 -0500

    added multi_io_frame.py

---
+++ prismio/multi_io_frame.py
@@ -0,0 +1,21 @@
+import sys
+import numpy as np
+import pandas as pd
+from io_frame import IOFrame
+
+
+class MultiIOFrame():
+    io_frames = []
+    
+    def __init__(self, io_frames):
+        self.io_frames = io_frames
+        self.set_log_dirs()
+        self.np = io_frames[0].np
+
+    def set_log_dirs(self):
+        self.log_dirs = []
+        for rdf in self.io_frames:
+            self.log_dirs.append(rdf.log_dir)
+
+    def get_log_dirs(self):
+        return self.log_dirs
