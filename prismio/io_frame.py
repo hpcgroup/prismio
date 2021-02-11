ddec30f6a638712cf4cc0efa7c5c8ec0caa2fec7
Yiheng Xu <yhxu@umd.edu>
2021-02-10 22:07:13 -0500

    added comments for new functions; fixed a bug related to groupby_aggregate

--- prismio/io_frame.py
+++ prismio/io_frame.py
@@ -70,32 +70,88 @@ class IOFrame:
         dataframe = dataframe.drop('index', axis=1)
         return IOFrame(dataframe)
 
-    def groupby_aggregate(self, groupby_columns, agg_function):
+    def groupby_aggregate(self, groupby_columns, agg_function, agg_columns=None):
         """
         Return a dataframe after groupby and aggregate operations on the dataframe of this IOFrame.
 
         Args:
             groupby_columns (list of strings): the column names the user want to groupby.
             agg_function (function or string): the function used for aggregation. For example, 'sum', 'count', etc.
+            agg_columns (list of strings): the columns the user want to aggregate, i.e., show up in the result.
 
         Return:
             A dataframe after groupby and aggregate operations on the dataframe of this IOFrame.
 
         """
         groupby_obj = self.dataframe.groupby(groupby_columns)
+        if agg_columns != None:
+            groupby_obj = groupby_obj[agg_columns]
         agg_dataframe = groupby_obj.agg(agg_function)
         return agg_dataframe
     
     def file_count(self, rank=None, agg_function=np.mean):
-        file_count = self.groupby_aggregate(['rank'], 'nunique')['file']
+        """
+        Depending on input arguments, return the number of files for each rank selected by the
+        user in the form of a Pandas Series or DataFrame. If agg_function is specified, then it 
+        will apply the function to the result.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            If rank == None and agg_function == None, it returns a Pandas DataFrame 
+            that contains the number of files for all ranks.
+            If rank != None and agg_function == None, it returns a Pandas DataFrame 
+            that contains the number of files for the listed ranks.
+            If rank == None and agg_function != None, it returns a number from applying
+            the function on the dataframe. For example, if agg_function = np.mean, it
+            returns the average number of files of all ranks.
+            If rank != None and agg_function != None, it returns a number from applying
+            the function on the dataframe for listed ranks. For example, if rank =
+            [1,3,5], agg_function = np.mean, it returns the average number of files 
+            of all rank 1, 3, 5.
+
+        """
+        file_count = self.groupby_aggregate(['rank'], 'nunique', agg_columns=['file'])
         if rank != None:
-            file_count = file_count.filter(items=rank)
+            file_count = file_count.loc[rank]
         if agg_function == None:
-            return file_count.to_dict()
+            return file_count
         else:
-            return agg_function(file_count.to_list())
+            return agg_function(file_count)
 
     def file_access_count(self, rank=None, agg_function=np.mean):
+        """
+        Depending on input arguments, return the number of accesses of each file in each rank
+        selected by the user in the form of a DataFrame. If agg_function is specified, then it 
+        will apply the function to the result.
+        The function first group the dataframe by file name. Then it goes through each group. 
+        And for each group, the function groups it by rank and aggregates to find the count for
+        each rank. Then it filters ranks the user wants, and put result to a dictionary. After
+        going through all file name groups. It use the dictionary to create a dataframe. If
+        agg_function is specified, it applies the function on the dataframe.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            If rank == None and agg_function == None, it returns a Pandas DataFrame in which
+            the columns are file names, rows are ranks, and the values are the number of accesses
+            for that file and rank.
+            If rank != None and agg_function == None, it returns a similar Pandas DataFrame but
+            with only user specified ranks (rows).
+            If rank == None and agg_function != None, it returns a Pandas Series that has the number
+            from applying the function on each file name across all ranks. For example, if agg_function = 
+            np.mean, it returns a series containing the average number of accesses for this file across 
+            all ranks.
+            If rank != None and agg_function != None, it returns a Pandas Series that has the number
+            from applying the function on each file name across selected ranks. For example, if agg_function = 
+            np.mean, rank = [1, 3, 5], it returns a series containing the average number of accesses for 
+            this file across rank 1, 3, 5.
+
+        """
         groupby_obj = self.dataframe.groupby(['file'])
         file_access_count = {}
         for key, group in groupby_obj:
@@ -110,6 +166,19 @@ class IOFrame:
             return file_access_count.apply(agg_function)
             
     def function_count(self, rank=None, agg_function=np.mean):
+        """
+        Identical to the previous one. Only instead of groupby file, it groupby function.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the number of function
+            calls for a function in selected ranks. Or avg/min/max accross selected ranks depending 
+            on the agg_function
+
+        """
         groupby_obj = self.dataframe.groupby(['function'])
         function_count = {}
         for key, group in groupby_obj:
@@ -124,6 +193,19 @@ class IOFrame:
             return function_count.apply(agg_function)
 
     def function_time(self, rank=None, agg_function=np.mean):
+        """
+        Identical to the previous one. Only instead of aggregating by count, it 
+        aggregating by sum of the time.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the total time of function
+            in selected ranks. Or avg/min/max accross selected ranks depending on the agg_function
+
+        """
         groupby_obj = self.dataframe.groupby(['function'])
         function_time = {}
         for key, group in groupby_obj:
@@ -138,7 +220,20 @@ class IOFrame:
             return function_time.apply(agg_function)
 
     def function_calls_by_library(self, rank=None, agg_function=np.mean):
-        
+        """
+        Count the number of function calls from mpi, hdf5 and posix. Same implementation to previous
+        ones. But it first check the library for each function call, and then groupby the library.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the number of function
+            calls of a library in selected ranks. Or avg/min/max accross selected ranks depending on 
+            the agg_function
+
+        """
         def check_library(function):
             if 'H5' in function:
                 return 'hdf5'
@@ -162,7 +257,21 @@ class IOFrame:
         else:
             return function_calls_by_library.apply(agg_function)
 
-    def print_trace(self, functions=None):
+    def plot_trace(self, functions=None):
+        """
+        Directly plot the trace of selected functions for all ranks.
+        First filter the selected functions and groupby functions. It then goes through each group
+        and construct x and y from tstart, tend and rank. The reason why adding those NaN is by doing
+        so matplotlib is able to plot segmented lines for each function call. Otherwise all lines will
+        be connected.
+
+        Args:
+            functions (list of strings): functions the user want to see in the trace.
+
+        Return:
+            None
+
+        """
         dataframe = self.dataframe
         if functions != None:
             dataframe = dataframe[dataframe['function'].isin(functions)]
@@ -178,10 +287,20 @@ class IOFrame:
         plt.show()
         plt.clf()
 
-    def print_io_sizes(self):
+    def plot_io_sizes(self):
+        """
+        Directly plot the number of io accesses with different sizes.
+
+        Args:
+            None.
+
+        Return:
+            None
+
+        """
         io_sizes = self.groupby_aggregate(['io_size'], 'count')['function']
         ax = io_sizes.plot.bar()
         Y = io_sizes.values
         for i, y in enumerate(Y):
             ax.text(i, y + 1, y, ha='center', va='bottom')
-        plt.savefig("mygraph.png")
+        plt.savefig("io_sizes.png")
