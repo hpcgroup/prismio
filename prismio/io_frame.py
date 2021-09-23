12be5cdeb10533581fab0feb5812b3d41fe08e9f
Yiheng Xu <yhxu@umd.edu>
2021-09-23 14:20:24 -0400

    add operations on IOFrame (#4)

--- prismio/io_frame.py
+++ prismio/io_frame.py
@@ -66,19 +66,237 @@ class IOFrame:
         dataframe = dataframe.drop('index', axis=1)
         return IOFrame(dataframe)
 
-    def groupby_aggregate(self, groupby_columns, agg_function):
+    def groupby_aggregate(self, groupby_columns, rank=None, agg_dict=None, drop=False):
         """
         Return a dataframe after groupby and aggregate operations on the dataframe of this IOFrame.
 
         Args:
-            groupby_columns (list of strings): the column names the user want to groupby.
-            agg_function (function or string): the function used for aggregation. For example, 'sum', 'count', etc.
+            groupby_columns (list of strings): the column names the user wants to groupby.
+            rank (list): Ranks the user wants to keep. Other ranks will be filtered out in the result.
+                If it is None, then keep all ranks.
+            agg_dict (dictionary): aggregation functions for some columns
+            drop: If true, drop columns not specified in agg_dict. Otherwise keep all columns in the result.
 
         Return:
             A dataframe after groupby and aggregate operations on the dataframe of this IOFrame.
 
         """
-        groupby_obj = self.dataframe.groupby(groupby_columns)
-        agg_dataframe = groupby_obj.agg(agg_function)
+        # default aggregation functions for all columns
+        default_agg_dict = {
+            'rank': lambda x: x.iloc[0],
+            'function_id': lambda x: x.iloc[0],
+            'function_name': lambda x: x.iloc[0],
+            'tstart': np.min,
+            'tend': np.max,
+            'time': np.sum,
+            'arg_count': lambda x: x.iloc[0],
+            'args': lambda x: x.iloc[0],
+            'return_value': lambda x: x.iloc[0],
+            'file_name': lambda x: x.iloc[0],
+            'io_volume': np.sum
+        }
+
+        # Filter out not specified ranks. Make a deep copy and groupby_agg on it,
+        # so self.dataframe is not changed.
+        if rank is not None:
+            dataframe = self.dataframe[self.dataframe['rank'].isin(rank)]
+            dataframe = dataframe.copy(deep=True)
+        else:
+            dataframe = self.dataframe
+
+        groupby_obj = dataframe.groupby(groupby_columns)
+
+        # if agg_dic is None, use the default agg_dict
+        if agg_dict is None:
+            agg_dataframe = groupby_obj.agg(default_agg_dict)
+            return agg_dataframe
+        
+        # if not, make sure columns contain keys in agg_dict
+        for key in agg_dict:
+            if key not in self.dataframe.columns:
+                raise KeyError("Specified column does not exist in the dataframe!")
+        
+        # if drop other columns, directly apply agg_dict
+        if drop:
+            agg_dataframe = groupby_obj.agg(agg_dict)
+        # else replace functions in default agg_dict with user specified ones,
+        # then apply default agg_dict
+        else:
+            for key in agg_dict:
+                default_agg_dict[key] = agg_dict[key]
+            agg_dataframe = groupby_obj.agg(default_agg_dict)
+        
         return agg_dataframe
     
+    def file_count(self, rank=None, agg_function=None):
+        """
+        Depending on input arguments, return the number of files for ranks selected by the
+        user in the form of a DataFrame. It contains the number of files touched (read or written) 
+        by these ranks. If agg_function is specified, then it will apply the function to 
+        the result.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            If rank == None and agg_function == None, it returns a Pandas DataFrame 
+            that contains the number of files for all ranks.
+            If rank != None and agg_function == None, it returns a Pandas DataFrame 
+            that contains the number of files for the listed ranks.
+            If rank == None and agg_function != None, it returns a number from applying
+            the function on the dataframe. For example, if agg_function = np.mean, it
+            returns the average number of files of all ranks.
+            If rank != None and agg_function != None, it returns a number from applying
+            the function on the dataframe for listed ranks. For example, if rank =
+            [1,3,5], agg_function = np.mean, it returns the average number of files 
+            of all rank 1, 3, 5.
+
+        """
+        # groupby rank, then count the number of unique file names
+        dataframe = self.groupby_aggregate(['rank'], rank=rank, agg_dict={'file_name': 'nunique'}, drop=True)
+        dataframe = dataframe.rename(columns={'file_name': 'file_count'})
+        
+        if agg_function == None:
+            return dataframe
+        # apply agg_function if it's not None
+        else:
+            return agg_function(dataframe)
+
+    def file_access_count(self, rank=None, agg_function=None):
+        """
+        Depending on input arguments, return the number of accesses of each file in each rank
+        selected by the user in the form of a DataFrame. If agg_function is specified, then it 
+        will apply the function to the result.
+        The function first group the dataframe by file name. Then it goes through each group. 
+        And for each group, the function groups it by rank and aggregates to find the count for
+        each rank. Then it filters ranks the user wants, and put result to a dictionary. After
+        going through all file name groups. It use the dictionary to create a dataframe. If
+        agg_function is specified, it applies the function on the dataframe.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            If rank == None and agg_function == None, it returns a Pandas DataFrame in which
+            the columns are file names, rows are ranks, and the values are the number of accesses
+            for that file and rank.
+            If rank != None and agg_function == None, it returns a similar Pandas DataFrame but
+            with only user specified ranks (rows).
+            If rank == None and agg_function != None, it returns a Pandas Series that has the number
+            from applying the function on each file name across all ranks. For example, if agg_function = 
+            np.mean, it returns a series containing the average number of accesses for this file across 
+            all ranks.
+            If rank != None and agg_function != None, it returns a Pandas Series that has the number
+            from applying the function on each file name across selected ranks. For example, if agg_function = 
+            np.mean, rank = [1, 3, 5], it returns a series containing the average number of accesses for 
+            this file across rank 1, 3, 5.
+
+        """
+        
+        # groupby file name and rank, then count the number of each file name
+        dataframe = self.groupby_aggregate(['file_name', 'rank'], rank=rank, agg_dict={'file_name': 'count'}, drop=True)
+        dataframe = dataframe.rename(columns={'file_name': 'file_access_count'})
+        
+        if agg_function is None:
+            return dataframe
+        # group by file names and apply agg_function over ranks if it's not None
+        else:
+            dataframe = dataframe.groupby(level=[0]).agg({'file_access_count': agg_function})
+            return dataframe
+            
+    def function_count(self, rank=None, agg_function=None):
+        """
+        Identical to the previous one. Only instead of groupby file, it groupby function.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the number of function
+            calls for a function in selected ranks. Or avg/min/max accross selected ranks depending 
+            on the agg_function
+
+        """
+        
+        # groupby function name and rank, then count the number of each function name
+        dataframe = self.groupby_aggregate(['function_name', 'rank'], rank=rank, agg_dict={'function_name': 'count'}, drop=True)
+        dataframe = dataframe.rename(columns={'function_name': 'function_count'})
+        
+        # group by function name and apply agg_function over ranks if it's not None
+        if agg_function is None:
+            return dataframe
+        else:
+            dataframe = dataframe.groupby(level=[0]).agg({'function_count': agg_function})
+            return dataframe
+
+    def function_time(self, rank=None, agg_function=None):
+        """
+        Identical to the previous one. Only instead of aggregating by count, it 
+        aggregating by sum of the time.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the total time of function
+            in selected ranks. Or avg/min/max accross selected ranks depending on the agg_function
+
+        """
+       
+        # groupby function name and rank, then sum the runtime 
+        dataframe = self.groupby_aggregate(['function_name', 'rank'], rank=rank, agg_dict={'time': 'sum'}, drop=True)
+        
+        if agg_function is None:
+            return dataframe
+        # group by function name and apply agg_function over ranks if it's not None
+        else:
+            dataframe = dataframe.groupby(level=[0]).agg({'time': agg_function})
+            return dataframe
+
+
+    def function_count_by_library(self, rank=None, agg_function=None):
+        """
+        Count the number of function calls from mpi, hdf5 and posix. Same implementation to previous
+        ones. But it first check the library for each function call, and then groupby the library.
+
+        Args:
+            rank (None or a list): user selected ranks to get file count.
+            agg_function: (function): aggregation function applying on the result.
+
+        Return:
+            Identical structure to the previous one, except the value here is the number of function
+            calls of a library in selected ranks. Or avg/min/max accross selected ranks depending on 
+            the agg_function
+
+        """
+        
+        # helper function to check library for a given function
+        def check_library(function):
+            if 'H5' in function:
+                return 'hdf5'
+            elif 'MPI' in function:
+                return 'mpi'
+            else:
+                return 'posix'
+
+        # check library for each row and put result into a new column 
+        self.dataframe['library'] = self.dataframe['function_name'].apply(lambda function: check_library(function))
+        
+        # groupby library name and rank, then count the number of functions in each library
+        dataframe = self.groupby_aggregate(['library', 'rank'], rank=rank, agg_dict={'library': 'count'}, drop=True)
+        
+        # drop the new column to maintain the original dataframe
+        self.dataframe.drop(['library'], axis=1)
+        
+        dataframe = dataframe.rename(columns={'library': 'library_call_count'})
+        
+        # group by library name and apply agg_function over ranks if it's not None
+        if agg_function is None:
+            return dataframe
+        else:
+            dataframe = dataframe.groupby(level=[0]).agg({'library_call_count': agg_function})
+            return dataframe
--- prismio/readers/recorder_reader.py
+++ prismio/readers/recorder_reader.py
@@ -60,15 +60,16 @@ class RecorderReader:
 
         records_as_dict = {
             'rank': [], 
-            'fid': [], 
-            'name': [], 
+            'function_id': [], 
+            'function_name': [], 
             'tstart': [],
             'tend': [],
             'time': [], 
             'arg_count': [],
             'args': [],
             'return_value': [],
-            'file': []
+            'file_name': [],
+            'io_volume': []
         }
 
         fd_to_filenames = [{0: "stdin", 1: "stdout", 2: "stderr"}] * self.reader.GM.total_ranks
@@ -78,6 +79,7 @@ class RecorderReader:
                 fd_to_filename = fd_to_filenames[rank]
                 function_args = record.args_to_strs()
                 func_name = self.reader.funcs[record.func_id]
+                io_size = None
                 if 'fdopen' in func_name:
                     fd = record.res
                     old_fd = int(function_args[0])
@@ -91,12 +93,20 @@ class RecorderReader:
                     filename = function_args[0]
                     fd_to_filename[fd] = filename
                 elif 'fwrite' in func_name or 'fread' in func_name:
+                    io_size = int(function_args[1]) * int(function_args[2])
                     fd = int(function_args[3])
                     if fd not in fd_to_filename:
                         filename = '__unknown__'
                     else: 
                         filename = fd_to_filename[fd]
-                elif 'seek' in func_name or 'close' in func_name or 'sync' in func_name or 'writev' in func_name or 'readv' in func_name or 'pwrite' in func_name or 'pread' in func_name or 'write' in func_name or 'read' in func_name or 'fprintf' in func_name:
+                elif 'seek' in func_name or 'close' in func_name or 'sync' in func_name or 'fprintf' in func_name:
+                    fd = int(function_args[0])
+                    if fd not in fd_to_filename:
+                        filename = '__unknown__'
+                    else: 
+                        filename = fd_to_filename[fd]
+                elif 'H5' not in func_name and ('writev' in func_name or 'readv' in func_name or 'pwrite' in func_name or 'pread' in func_name or 'write' in func_name or 'read' in func_name):
+                    io_size = int(function_args[2])
                     fd = int(function_args[0])
                     if fd not in fd_to_filename:
                         filename = '__unknown__'
@@ -106,15 +116,16 @@ class RecorderReader:
                     filename = None    
 
                 records_as_dict['rank'].append(rank)
-                records_as_dict['fid'].append(record.func_id)
-                records_as_dict['name'].append(func_name)
+                records_as_dict['function_id'].append(record.func_id)
+                records_as_dict['function_name'].append(func_name)
                 records_as_dict['tstart'].append(record.tstart)
                 records_as_dict['tend'].append(record.tend)
                 records_as_dict['time'].append(record.tend - record.tstart)
                 records_as_dict['arg_count'].append(record.arg_count)
                 records_as_dict['args'].append(function_args)
                 records_as_dict['return_value'].append(record.res)
-                records_as_dict['file'].append(filename) 
+                records_as_dict['file_name'].append(filename) 
+                records_as_dict['io_volume'].append(io_size) 
 
         dataframe = pd.DataFrame.from_dict(records_as_dict)
 
